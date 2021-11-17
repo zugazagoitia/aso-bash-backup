eb7d3fd0f12bf29cf454259aacf67415a17e293e
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2021-11-17 12:52:03 +0100

    Added checking for write and read permissions.
    Fixes #1,Fixes  #2, Fixes #3

--- backup-cron.sh
+++ backup-cron.sh
@@ -15,10 +15,33 @@ log_error(){
 
 
 DIRECTORY=$1
+
+#Check if the backup directory exists and is a directory, then test for write permissions
+if [ ! -d "$BACKUP_FOLDER" ]; 
+then
+    if [ ! -w "$BACKUP_FOLDER" ];
+    then
+        echo "The folder $BACKUP_FOLDER does not exist and you don't have the permission to create it."
+        log_error "The folder $BACKUP_FOLDER does not exist and you don't have the permission to create it."
+        sleep 3
+        exit 1
+    fi
+fi
+
+
+#Check if the directory exists and is a directory, then test for read permissions
 if [ ! -d "$DIRECTORY" ]; 
 then
     log_error "Invalid directory: $DIRECTORY"
     exit 1 
+else
+    if [ ! -r "$DIRECTORY" ];
+    then
+        echo "You don't have the permission to write to $DIRECTORY."
+        log_error "You don't have the permission to write to $DIRECTORY."
+        sleep 3
+        return
+    fi
 fi
 
 FILENAME="$(basename "$DIRECTORY")-$(date '+%y%m%d-%H%M').tgz"
